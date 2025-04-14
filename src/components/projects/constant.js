aaa24123d43a1c5fd30d35ac5a3469595e0ca33a
asharfarooq09 <farooqashar09@gmail.com>
2025-04-14 18:01:42 +0530

    add projects

--- src/components/projects/constant.js
+++ src/components/projects/constant.js
@@ -1,10 +1,16 @@
 import Shopper from "../../assets/shopper.png";
 import CRM from "../../assets/CRM.png";
-import newspaperApp from "../../assets/newspaperApp.png";
+import carFinder from "../../assets/car-finder.png";
+import Commerce from "../../assets/e-commerce.png";
+import Calender from "../../assets/Calender.png";
+import cryptoWeather from "../../assets/crypto-weather.png";
 
 
 export const Allprojects = [
   {image: Shopper, link:  "https://shopper00.netlify.app"},
+  {image: carFinder, link:  "https://carfindrapp.netlify.app/"},
+  {image: Commerce, link:  "https://e-commerce-react0.netlify.app"},
+  {image: Calender, link:  "https://calenderevnt.netlify.app/"},
+  {image: cryptoWeather, link:  "https://cryptoweathernexusassignment.netlify.app"},
   {image: CRM, link:  "https://ticketingcrm.netlify.app"},
-  {image: newspaperApp, link:  ""},
 ]
